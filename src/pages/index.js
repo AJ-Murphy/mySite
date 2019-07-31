b32cd18f6bb8d44f0440dc58b47b9512b995d0f1
AJ Murphy <aj.murphy@outlook.com>
2019-07-31 13:21:06 +0100

    Removed unsued imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
-// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
