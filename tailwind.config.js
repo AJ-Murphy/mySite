d56e19e3b304e2401a8d110a3b2e165356cca07b
AJ Murphy <aj.murphy@outlook.com>
2019-07-31 13:17:15 +0100

    Updated custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      white: "#ececec",
+      black: "#000025",
+      red: "#A7425D",
+      blue: "#385170",
+      darkBlue: "#142d4c",
+    },
+    extend: {},
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
