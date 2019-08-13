8f56e9721a0e52767b1024e61e755b94c4ca75b0
Adam <aj.murphy@outlook.com>
2019-08-13 17:14:50 +0100

    Fixed with and ready for image input

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,19 @@
 import React from "react"
 import PropTypes from "prop-types"
+// import Img from "gatsby-image"
 
 const Card = ({ headingText, paragraphText, image, imageText }) => {
   return (
-    <div className="max-w-sm bg-white text-initial rounded overflow-hidden shadow-lg mx-6">
-      <img className="w-full" src={image} alt={imageText} />
+    <div className="max-w-xs bg-white text-initial rounded overflow-hidden shadow-lg mx-6">
+      {/* <Img fixed={image} /> */}
+      <img src={image} />
       <div className="px-6 py-4">
-        <div className="font-bold mb-2">{headingText}</div>
-        <p className="text-base">{paragraphText}</p>
+        <div className="font-bold text-xl mb-2">
+          <a target="_blank" href="#top" title={imageText}>
+            {headingText}
+          </a>
+        </div>
+        <p className="text-sm mb-2">{paragraphText}</p>
       </div>
     </div>
   )
