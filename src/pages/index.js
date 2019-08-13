4108889ed92db27596c93e1ded0a74596ba4a4bb
Adam <aj.murphy@outlook.com>
2019-08-13 17:23:38 +0100

    added setup for battleship image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import * as Icon from "react-feather"
-import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Nav from "../components/nav"
@@ -13,38 +13,55 @@ import Contact from "../components/contactForm"
 
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Nav>
-      <NavLinks link="#hero" icon={<Icon.Home />} />
-      <NavLinks link="#about" icon={<Icon.User />} />
-      <NavLinks link="#portfolio" icon={<Icon.Code />} />
-      <NavLinks link="#contact" icon={<Icon.Mail />} />
-    </Nav>
-    <Hero
-      headingText="Adam J Murphy"
-      subHeadingText="Full-stack web developer"
-      buttonText="Contact"
-      buttonLink="#contact"
-    />
-    <Section
-      headingText="About"
-      paragraphText="Web developer with 2 years experiance"
-    />
-    <Section headingText="Portfolio">
-      <Cards>
-        <Card
-          headingText="Battleships"
-          paragraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non autem hoc: igitur ne illud quidem. Si enim ad populum me vocas, eum. Zenonis est, inquam, hoc Stoici. Sed ad bona praeterita redeamus. Duo Reges: constructio interrete. Summum ením bonum exposuit vacuitatem doloris; "
-          image="https://via.placeholder.com/450x250"
-        />
-      </Cards>
-    </Section>
-    <Section headingText="Contact">
-      <Contact />
-    </Section>
-  </Layout>
-)
+const IndexPage = () => {
+  // const battleship = useStaticQuery(graphql`
+  //   query {
+  //     file(relativePath: { eq: "battleship.jpg" }) {
+  //       childImageSharp {
+  //         # Specify a fixed image and fragment.
+  //         # The default width is 400 pixels
+  //         fixed(width: 400, height: 300) {
+  //           ...GatsbyImageSharpFixed
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Nav>
+        <NavLinks link="#hero" icon={<Icon.Home />} />
+        <NavLinks link="#about" icon={<Icon.User />} />
+        <NavLinks link="#portfolio" icon={<Icon.Code />} />
+        <NavLinks link="#contact" icon={<Icon.Mail />} />
+      </Nav>
+      <Hero
+        headingText="Adam J Murphy"
+        subHeadingText="Full-stack web developer"
+        buttonText="Contact"
+        buttonLink="#contact"
+      />
+      <Section
+        headingText="About"
+        paragraphText="Web developer with 2 years experiance"
+      />
+      <Section headingText="Portfolio">
+        <Cards>
+          <Card
+            headingText="Battleships"
+            paragraphText="Small game of Battleships made with CSS Grid and JavaScript!"
+            // image={battleship.file.childImageSharp.fixed}
+            image="https://via.placeholder.com/400"
+          />
+        </Cards>
+      </Section>
+      <Section headingText="Contact">
+        <Contact />
+      </Section>
+    </Layout>
+  )
+}
 
 export default IndexPage
