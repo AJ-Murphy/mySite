8cb324285bd369a9adff7db9f2e2d992cad8141c
Adam <aj.murphy@outlook.com>
2019-08-27 17:35:54 +0100

    Updated contact section

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -14,13 +14,13 @@ const Contact = () => {
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            className="block uppercase tracking-wide text-initial text-xs font-bold mb-2"
             for="first-name"
           >
             First Name
           </label>
           <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            className="appearance-none block w-full bg-gray-200 text-inherit border border-black rounded py-3 px-4 mb-3 leading-tight"
             id="first-name"
             type="text"
           />
@@ -33,7 +33,7 @@ const Contact = () => {
             Last Name
           </label>
           <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-full bg-gray-200 text-inherit border border-black rounded py-3 px-4 mb-3 leading-tight"
             id="last-name"
             type="text"
           />
@@ -48,7 +48,7 @@ const Contact = () => {
             Message
           </label>
           <textarea
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded mb-2 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            className="appearance-none block w-full bg-gray-200 text-inherit border border-black rounded mb-2 py-3 px-4 leading-tight focus:outline-none"
             name="message"
             id="message"
             type="text"
@@ -56,13 +56,13 @@ const Contact = () => {
           />
           <div className="flex items-center justify-between">
             <button
-              className="bg-transparent hover:bg-lightBlue text-black font-semibold hover:text-white py-2 px-4 border-2 border-black hover:border-lightBlue rounded cursor-pointer"
+              className="bg-gray-200 hover:bg-lightBlue text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-lightBlue rounded cursor-pointer"
               type="button"
             >
               Send
             </button>
             <input
-              className="bg-transparent hover:bg-lightBlue text-black font-semibold hover:text-white py-2 px-4 border-2 border-black hover:border-lightBlue rounded cursor-pointer"
+              className="bg-gray-200 hover:bg-lightBlue text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-lightBlue rounded cursor-pointer"
               type="reset"
               value="Clear"
             />
