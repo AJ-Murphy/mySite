b083b9103ce73af4515d9cc9231194e5b31307ce
Adam <aj.murphy@outlook.com>
2019-08-28 15:36:02 +0100

    Added new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#000025`,
         theme_color: `#000025`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ajmurphy-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
